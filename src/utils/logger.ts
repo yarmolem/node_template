ca250e71250ac0cbb918f3b3c7d7f34611efc55f
Cristian Gonzalez <kazragore96@gmail.com>
2023-05-21 14:36:18 -0500

    feat: Configurados archivos logs

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,20 +1,30 @@
 import winston from 'winston'
+import TzDate from './TzDate'
+
+function timezoned(): string {
+  return new TzDate().lima?.format('YYYY-MM-DD HH:mm:ss') ?? ''
+}
 
 const logger: winston.Logger = winston.createLogger({
   level: 'silly',
   levels: winston.config.npm.levels,
   format: winston.format.combine(
-    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    winston.format.timestamp({ format: timezoned }),
     winston.format.errors({ stack: true }),
     winston.format.splat(),
     winston.format.json()
   ),
   transports: [
+    new winston.transports.File({
+      level: 'info',
+      filename: 'logs/info.log'
+    }),
+    new winston.transports.File({
+      level: 'error',
+      filename: 'logs/error.log'
+    }),
     new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.cli(),
-        winston.format.splat()
-      )
+      format: winston.format.combine(winston.format.cli(), winston.format.splat())
     })
   ]
 })
